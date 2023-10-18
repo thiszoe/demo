06dee4892aa5ecde3958dd148927100eee752005
thiszoe <z.oellinger@students.htl-leonding.ac.at>
2023-10-18 15:16:32 +0200

    init

--- src/main/java/at/ac/htl/leonding/demo/App.java
+++ src/main/java/at/ac/htl/leonding/demo/App.java
@@ -10,4 +10,8 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+
+    public int add(int a, int b){
+       return a + b;
+    }
 }
--- src/test/java/at/ac/htl/leonding/demo/AppTest.java
+++ src/test/java/at/ac/htl/leonding/demo/AppTest.java
@@ -1,5 +1,6 @@
 package at.ac.htl.leonding.demo;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -16,5 +17,18 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @ Test
+    public void given_add_function_when_adding_2_and_3_then_result_5(){
+        var app = new App();
+
+        var result = app.add(2,3);
+
+        assertEquals(null, 5, 2, 3);
+
+
+
+        //int result = 
     }
 }
