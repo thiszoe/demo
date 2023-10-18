60840ff76ba473b7a88cc4a171fca53aa8607f34
thiszoe <z.oellinger@students.htl-leonding.ac.at>
2023-10-18 15:17:34 +0200

    init

--- src/main/java/at/ac/htl/leonding/demo/App.java
+++ src/main/java/at/ac/htl/leonding/demo/App.java
@@ -11,7 +11,5 @@ public class App
         System.out.println( "Hello World!" );
     }
 
-    public int add(int a, int b){
-       return a + b;
-    }
+  
 }
--- src/test/java/at/ac/htl/leonding/demo/AppTest.java
+++ src/test/java/at/ac/htl/leonding/demo/AppTest.java
@@ -19,16 +19,5 @@ public class AppTest
         assertTrue( true );
     }
 
-    @ Test
-    public void given_add_function_when_adding_2_and_3_then_result_5(){
-        var app = new App();
-
-        var result = app.add(2,3);
-
-        assertEquals(null, 5, 2, 3);
-
-
-
-        //int result = 
-    }
+ 
 }
