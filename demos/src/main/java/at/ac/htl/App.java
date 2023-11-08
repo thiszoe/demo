ebfe9741baa0099ed3b505456cf933244aa063d4
thiszoe <z.oellinger@students.htl-leonding.ac.at>
2023-11-08 15:06:39 +0100

    init

--- demos/src/main/java/at/ac/htl/App.java
+++ demos/src/main/java/at/ac/htl/App.java
@@ -11,8 +11,10 @@ public class App
     }
 
     public int getSum(){
-        return IntStream.rangeClosed(1, 10)
-        .map(n -> n * n)
-        .sum();
+        var sum = 0;
+        for (var i = 1; i <= 10; i++){
+            sum += i * i;
+        }
+        return sum;
     }
 }
--- demos/src/test/java/at/ac/htl/AppTest.java
+++ demos/src/test/java/at/ac/htl/AppTest.java
@@ -1,17 +1,13 @@
 package at.ac.htl;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
+
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+    
     @Test
     public void WhenGetSumThenResultIs377()
     {
